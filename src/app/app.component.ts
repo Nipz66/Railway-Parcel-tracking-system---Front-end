da5a073c3a122d734a2e9d626e27bdded77bb534
Nipun Anupama <anupamaalahakoon66@gmail.com>
2024-11-19 21:21:31 +0530

    Contact-Home section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { StaffNavbarComponent } from "./staff/staff-navbar/staff-navbar.component";
 import { ParcelReportComponent } from './staff/parcel-report/parcel-report.component';
 import { StaffRegistationComponent } from "./admin/staff-registation/staff-registation.component";
+import { NavBarComponent } from "./common/nav-bar/nav-bar.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, StaffNavbarComponent, ParcelReportComponent, StaffRegistationComponent],
+  imports: [RouterOutlet, StaffNavbarComponent, ParcelReportComponent, StaffRegistationComponent, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/common/nav-bar/nav-bar.component.ts
+++ src/app/common/nav-bar/nav-bar.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { FeaturesComponent } from "../features/features.component";
 import { AboutComponent } from "../about/about.component";
 import { FAQsComponent } from "../faqs/faqs.component";
+import { ContactComponent } from "../contact/contact.component";
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [FeaturesComponent, AboutComponent, FAQsComponent],
+  imports: [FeaturesComponent, AboutComponent, FAQsComponent, ContactComponent],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
